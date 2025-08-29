55d8a2caea435dfc71198e2f35f29f41deff9de9
Anton <a.chertok@akatech.com.ua>
2025-08-29 14:08:59 +0100

    fix: linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
+
       sourceType: "commonjs",
       parserOptions: {
         projectService: true,
@@ -45,5 +46,5 @@ export default tseslint.config(
         },
       ],
     },
-  }
+  },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-const a = "";
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT = process.env.PORT ?? 9000;
+
+  await app.listen(PORT, () =>
+    console.log(`\x1b[34mServer started on port = ${PORT}\x1b[0m`),
+  );
 }
 bootstrap();
